a1b1abc17c57b960a31edd0d79b336f8b06c5cfe
jupitters <sampaiobale@gmail.com>
2025-12-02 16:48:35 -0300

    init

---
+++ src/main/java/com/jupitters/securedoc/SecuredocApplication.java
@@ -0,0 +1,13 @@
+package com.jupitters.securedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupitters/securedoc/SecuredocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.securedoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
