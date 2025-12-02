f8122c9f39756083e00a15ac55e5dc7a5c77f308
jupitters <sampaiobale@gmail.com>
2025-12-02 16:48:56 -0300

    super class auditable

--- src/main/java/com/jupitters/securedoc/entity/Auditable.java
+++ src/main/java/com/jupitters/securedoc/entity/Auditable.java
@@ -1,4 +1,24 @@
 package com.jupitters.securedoc.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.AlternativeJdkIdGenerator;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt"}, allowGetters = true)
 public abstract class Auditable {
+    private Long id;
+    private String referenceId = new AlternativeJdkIdGenerator().generateId().toString();
+    private Long createdBy;
+    private Long updatedBy;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
