ef0583630d411d7b61448861893bc21ac152f848
jupitters <sampaiobale@gmail.com>
2025-12-02 23:19:33 -0300

    role model

--- src/main/java/com/jupitters/securedoc/entity/Role.java
+++ src/main/java/com/jupitters/securedoc/entity/Role.java
@@ -1,4 +1,18 @@
 package com.jupitters.securedoc.entity;
 
-public class Role {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class Role extends Auditable{
+    private String name;
+    private String authorities;
 }
