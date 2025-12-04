1c4a1d9e67aa6d6320985d5ac06d3bfe25ff51e6
jupitters <sampaiobale@gmail.com>
2025-12-03 22:52:57 -0300

    enum authority

--- src/main/java/com/jupitters/securedoc/enums/Authority.java
+++ src/main/java/com/jupitters/securedoc/enums/Authority.java
@@ -1,4 +1,14 @@
 package com.jupitters.securedoc.enums;
 
 public enum Authority {
+    USER(""),
+    ADMIN(""),
+    SUPER_ADMIN(""),
+    MANAGER("");
+
+    private final String value;
+
+    Authority(String value) {
+        this.value = value;
+    }
 }
