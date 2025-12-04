ed2a701fd99a96e3a17380c846d314dc3a075cb3
jupitters <sampaiobale@gmail.com>
2025-12-03 22:58:26 -0300

    constants

--- src/main/java/com/jupitters/securedoc/constant/Constants.java
+++ src/main/java/com/jupitters/securedoc/constant/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String USER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
     public static final String ADMIN_AUTHORITIES = "user:create,user:read,user:update,document:create,document:read,document:update,document:delete";
     public static final String SUPER_ADMIN_AUTHORITIES = "user:create,user:read,user:update,user:delete,document:create,document:read,document:update,document:delete";
-
+    public static final String MANAGER_AUTHORITIES = "document:create,document:read,document:update,document:delete";
 }
