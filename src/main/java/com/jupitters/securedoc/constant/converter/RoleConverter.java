8dd1fafc85f908ccb0f6326e3316e98e7b258eac
jupitters <sampaiobale@gmail.com>
2025-12-03 23:02:47 -0300

    role converter

--- src/main/java/com/jupitters/securedoc/constant/converter/RoleConverter.java
+++ src/main/java/com/jupitters/securedoc/constant/converter/RoleConverter.java
@@ -1,4 +1,18 @@
 package com.jupitters.securedoc.constant.converter;
 
-public class RoleConverter {
+import com.jupitters.securedoc.enums.Authority;
+import jakarta.persistence.AttributeConverter;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+public class RoleConverter implements AttributeConverter<Authority, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Authority authority) {
+        return "";
+    }
+
+    @Override
+    public Authority convertToEntityAttribute(String s) {
+        return null;
+    }
 }
