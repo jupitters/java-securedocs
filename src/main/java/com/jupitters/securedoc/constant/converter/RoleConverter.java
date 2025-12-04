128e775f6cbdcecb761633f1392d6ea331c877e8
jupitters <sampaiobale@gmail.com>
2025-12-03 23:07:46 -0300

    role converter

--- src/main/java/com/jupitters/securedoc/constant/converter/RoleConverter.java
+++ src/main/java/com/jupitters/securedoc/constant/converter/RoleConverter.java
@@ -16,14 +16,5 @@ public class RoleConverter implements AttributeConverter<Authority, String> {
         return authority.getValue();
     }
 
-    @Override
-    public Authority convertToEntityAttribute(String s) {
-        if(s == null){
-            return null;
-        }
-        return Stream.of(Authority.values())
-                .filter(authority -> authority.getValue().equals(s))
-                .findFirst()
-                .orElseThrow(IllegalAccessError::new);
-    }
+
 }
