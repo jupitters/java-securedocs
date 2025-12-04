4cef1b5096dd70d88cf2f041d0fa8e2c1314ce87
jupitters <sampaiobale@gmail.com>
2025-12-04 19:03:12 -0300

    credential model

--- src/main/java/com/jupitters/securedoc/entity/Credential.java
+++ src/main/java/com/jupitters/securedoc/entity/Credential.java
@@ -18,11 +18,7 @@ import org.hibernate.annotations.OnDeleteAction;
 @Entity
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Credential extends Auditable {
-    private String password;
-    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private User user;
+    
 
     public Credential(User user, String password) {
         this.user = user;
