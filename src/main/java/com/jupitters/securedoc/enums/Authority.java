61a5d55a8f431816195b237de873e0b48ef982b3
jupitters <sampaiobale@gmail.com>
2025-12-03 23:00:00 -0300

    authority enum

--- src/main/java/com/jupitters/securedoc/enums/Authority.java
+++ src/main/java/com/jupitters/securedoc/enums/Authority.java
@@ -1,10 +1,12 @@
 package com.jupitters.securedoc.enums;
 
+import static com.jupitters.securedoc.constant.Constants.*;
+
 public enum Authority {
-    USER(""),
-    ADMIN(""),
-    SUPER_ADMIN(""),
-    MANAGER("");
+    USER(USER_AUTHORITIES),
+    ADMIN(ADMIN_AUTHORITIES),
+    SUPER_ADMIN(SUPER_ADMIN_AUTHORITIES),
+    MANAGER(MANAGER_AUTHORITIES);
 
     private final String value;
 
