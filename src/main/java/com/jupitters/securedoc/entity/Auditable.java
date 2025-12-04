6ec20754bb251bb1eed8f1664e2f44c6520cdfe8
jupitters <sampaiobale@gmail.com>
2025-12-04 19:13:36 -0300

    auditable model

--- src/main/java/com/jupitters/securedoc/entity/Auditable.java
+++ src/main/java/com/jupitters/securedoc/entity/Auditable.java
@@ -19,7 +19,23 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = { "createdAt", "updatedAt"}, allowGetters = true)
 public abstract class Auditable {
-
+    @Id
+    @SequenceGenerator(name = "primary_key_seq", sequenceName = "primary_key_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "primary_key_seq")
+    @Column(name = "id", updatable = false)
+    private Long id;
+    private String referenceId = new AlternativeJdkIdGenerator().generateId().toString();
+    @NotNull
+    private Long createdBy;
+    @NotNull
+    private Long updatedBy;
+    @NotNull
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 
     @PrePersist
     public void beforePersists(){
