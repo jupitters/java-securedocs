2cc1197f98d92099b78255d257366c58853711c5
jupitters <sampaiobale@gmail.com>
2025-12-05 16:44:56 -0300

    email utils

--- src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
+++ src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
@@ -1,4 +1,14 @@
 package com.jupitters.securedoc.utils;
 
 public class EmailUtils {
+    public String getResetPasswordMessage(String name, String host, String token) {
+        return "Hello" + name + ",\n\n" +
+                "Your new account has been created. " +
+                "Please click on the link below to verify your account.\n\n" +
+                getVerificationUrl(host, token) +
+                "\n\nThe Support Team";
+    }
+
+    public String getVerificationUrl(String host, String token) {
+    }
 }
