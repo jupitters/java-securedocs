6ac280e57aec4dd20b3d9d5106f9a865028f9baf
jupitters <sampaiobale@gmail.com>
2025-12-05 16:34:57 -0300

    email service impl

--- src/main/java/com/jupitters/securedoc/service/EmailService.java
+++ src/main/java/com/jupitters/securedoc/service/EmailService.java
@@ -1,6 +1,6 @@
 package com.jupitters.securedoc.service;
 
 public interface EmailService {
-    void sendNewAccountEmail(String name, String to, String token);
-    void sendPasswordResetEmail(String name, String to, String token);
+    void sendNewAccountEmail(String name, String emailTo, String token);
+    void sendPasswordResetEmail(String name, String emailTo, String token);
 }
--- src/main/java/com/jupitters/securedoc/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jupitters/securedoc/service/impl/EmailServiceImpl.java
@@ -1,11 +1,38 @@
 package com.jupitters.securedoc.service.impl;
 
+import com.jupitters.securedoc.exception.ApiException;
 import com.jupitters.securedoc.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
-    @Override
-    public void sendNewAccountEmail(String name, String to, String token) {
+    public static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
+    private final JavaMailSender sender;
+    @Value("${spring.mail.verify.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String fromMail;
 
+    @Override
+    public void sendNewAccountEmail(String name, String emailTo, String token) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
+            message.setFrom(fromMail);
+            message.setTo(emailTo);
+            message.setText(getEmailMessage(name, host, token));
+            sender.send(message);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ApiException("Unable to send mail");
+        }
     }
 
     @Override
