7c92a1a0ec9bd0ffef436501a55678edff16fa44
jupitters <sampaiobale@gmail.com>
2025-12-05 16:21:33 -0300

    email service impl

--- src/main/java/com/jupitters/securedoc/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jupitters/securedoc/service/impl/EmailServiceImpl.java
@@ -1,4 +1,15 @@
 package com.jupitters.securedoc.service.impl;
 
-public class EmailServiceImpl {
+import com.jupitters.securedoc.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void sendNewAccountEmail(String name, String to, String token) {
+
+    }
+
+    @Override
+    public void sendPasswordResetEmail(String name, String to, String token) {
+
+    }
 }
