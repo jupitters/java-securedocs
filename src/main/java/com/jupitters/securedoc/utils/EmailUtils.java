641daa5b2e446ab7faf9abe4cd4cb5fec7089c79
jupitters <sampaiobale@gmail.com>
2025-12-06 17:42:03 -0300

    email service

--- src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
+++ src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
@@ -17,14 +17,6 @@ public class EmailUtils {
                 "\n\nThe Support Team";
     }
 
-    public static String getEmailMessage(String name, String host, String token) {
-        return "Hello" + name + ",\n\n" +
-                "Your new account has been created. " +
-                "Please click on the link below to verify your account.\n\n" +
-                getVerificationUrl(host, token) +
-                "\n\nThe Support Team";
-    }
-
     public static String getVerificationUrl(String host, String token) {
         return host + "/verify/account?token=" + token;
     }
