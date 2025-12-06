07727a312e378d9624a9ec1f74bd8f91850e0068
jupitters <sampaiobale@gmail.com>
2025-12-06 17:40:23 -0300

    email service

--- src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
+++ src/main/java/com/jupitters/securedoc/utils/EmailUtils.java
@@ -9,16 +9,24 @@ public class EmailUtils {
                 "\n\nThe Support Team";
     }
 
-    public static String getVerificationUrl(String host, String token) {
-        return host + "/verify/account?token=" + token;
-    }
-
     public static String getResetPasswordMessage(String name, String host, String token) {
         return "Hello" + name + ",\n\n" +
                 "Your new account has been created. " +
                 "Please click on the link below to verify your account.\n\n" +
                 getVerificationUrl(host, token) +
                 "\n\nThe Support Team";
+    }
+
+    public static String getEmailMessage(String name, String host, String token) {
+        return "Hello" + name + ",\n\n" +
+                "Your new account has been created. " +
+                "Please click on the link below to verify your account.\n\n" +
+                getVerificationUrl(host, token) +
+                "\n\nThe Support Team";
+    }
+
+    public static String getVerificationUrl(String host, String token) {
+        return host + "/verify/account?token=" + token;
     }
 
     public static String getPasswordResetUrl(String host, String token) {
