f010f28c28560c606515a4d846ba295e55c26c7f
jupitters <sampaiobale@gmail.com>
2025-12-06 17:59:57 -0300

    user event

---
+++ src/main/java/com/jupitters/securedoc/event/listener/UserEventListener.java
@@ -0,0 +1,26 @@
+package com.jupitters.securedoc.event.listener;
+
+import com.jupitters.securedoc.event.UserEvent;
+import com.jupitters.securedoc.service.EmailService;
+import com.jupitters.securedoc.utils.EmailUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserEventListener {
+    private final EmailService emailService;
+
+    @EventListener
+    public void onUserEvent(UserEvent event) {
+        switch (event.getType()) {
+            case REGISTRATION -> emailService.sendNewAccountEmail(event.getUser().getFirstName(),
+                    event.getUser().getEmail(),
+                    (String)event.getData().get("key"));
+            case RESET_PASSWORD -> emailService.sendNewAccountEmail(event.getUser().getFirstName(),
+                    event.getUser().getEmail(),
+                    (String)event.getData().get("key"));
+        }
+    }
+}
