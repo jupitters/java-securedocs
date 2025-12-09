71ca1e01eb8cdc7b718dd4bee4012cff220ed667
jupitters <sampaiobale@gmail.com>
2025-12-09 10:01:29 -0300

    role repository

--- src/main/java/com/jupitters/securedoc/repository/RoleRepository.java
+++ src/main/java/com/jupitters/securedoc/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package com.jupitters.securedoc.repository;
 
-public interface RoleRepository {
+import com.jupitters.securedoc.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
