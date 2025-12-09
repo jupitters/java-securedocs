da9edbd246675420bb909e8fed35babc5e413dbd
jupitters <sampaiobale@gmail.com>
2025-12-09 10:07:35 -0300

    user  repository

--- src/main/java/com/jupitters/securedoc/repository/UserRepository.java
+++ src/main/java/com/jupitters/securedoc/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.jupitters.securedoc.repository;
 
-public interface UserRepository {
+import com.jupitters.securedoc.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
