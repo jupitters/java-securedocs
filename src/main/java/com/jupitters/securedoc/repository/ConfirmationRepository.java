8866d6642166f448b3300bdf7c5d82e6854d5402
jupitters <sampaiobale@gmail.com>
2025-12-09 10:07:01 -0300

    confirmation  repository

--- src/main/java/com/jupitters/securedoc/repository/ConfirmationRepository.java
+++ src/main/java/com/jupitters/securedoc/repository/ConfirmationRepository.java
@@ -1,9 +1,14 @@
 package com.jupitters.securedoc.repository;
 
 import com.jupitters.securedoc.entity.Confirmation;
+import com.jupitters.securedoc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmationRepository  extends JpaRepository<Confirmation, Long> {
+    Optional<Confirmation> findByKey(String key);
+    Optional<Confirmation> findByUser(User user);
 }
