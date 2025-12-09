8dacd9526f17844a76a00ff6a8d6314f71240e85
jupitters <sampaiobale@gmail.com>
2025-12-09 11:01:36 -0300

    response

--- src/main/java/com/jupitters/securedoc/resource/UserResource.java
+++ src/main/java/com/jupitters/securedoc/resource/UserResource.java
@@ -1,4 +1,23 @@
 package com.jupitters.securedoc.resource;
 
+import com.jupitters.securedoc.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/user")
+@RequiredArgsConstructor
 public class UserResource {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> saveUser(@RequestBody @Valid UserRequest user, HttpServletRequest request) {
+
+    }
 }
