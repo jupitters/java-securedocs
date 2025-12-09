61cf58f4f4875bd2876537b4c0947858ef99f387
jupitters <sampaiobale@gmail.com>
2025-12-09 11:55:42 -0300

    user request

--- src/main/java/com/jupitters/securedoc/dtorequest/UserRequest.java
+++ src/main/java/com/jupitters/securedoc/dtorequest/UserRequest.java
@@ -1,4 +1,17 @@
 package com.jupitters.securedoc.dtorequest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties
 public class UserRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String bio;
+    private String phone;
 }
