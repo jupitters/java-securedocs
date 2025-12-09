52a5d6cbc4cd1037443fd12f1b4fd54db358acc9
jupitters <sampaiobale@gmail.com>
2025-12-09 10:22:15 -0300

    user  service

---
+++ src/main/java/com/jupitters/securedoc/service/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.jupitters.securedoc.service.impl;
+
+import com.jupitters.securedoc.entity.Confirmation;
+import com.jupitters.securedoc.entity.Credential;
+import com.jupitters.securedoc.enums.EventType;
+import com.jupitters.securedoc.event.UserEvent;
+import com.jupitters.securedoc.repository.ConfirmationRepository;
+import com.jupitters.securedoc.repository.CredentialRepository;
+import com.jupitters.securedoc.repository.RoleRepository;
+import com.jupitters.securedoc.repository.UserRepository;
+import com.jupitters.securedoc.service.UserService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Transactional(rollbackOn =  Exception.class)
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final CredentialRepository credentialRepository;
+    private final ConfirmationRepository confirmationRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final ApplicationEventPublisher publisher;
+
+    @Override
+    public void createUser(String firstName, String lastName, String email, String password) {
+        var user = userRepository.save(createNewUser(firstName, lastName, email));
+        var credential = new Credential(user, password);
+        credentialRepository.save(credential);
+        var confirmation = new Confirmation(user);
+        confirmationRepository.save(confirmation);
+        publisher.publishEvent(new UserEvent(user, EventType.REGISTRATION, Map.of("key", confirmation.getKey())));
+    }
+}
