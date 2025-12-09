7be7460d93636aad866d1f397197ae071e4accd1
jupitters <sampaiobale@gmail.com>
2025-12-09 10:04:59 -0300

    credential repository

--- src/main/java/com/jupitters/securedoc/repository/CredentialRepository.java
+++ src/main/java/com/jupitters/securedoc/repository/CredentialRepository.java
@@ -1,4 +1,12 @@
 package com.jupitters.securedoc.repository;
 
-public interface CredentialRepository {
+import com.jupitters.securedoc.entity.Credential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CredentialRepository extends JpaRepository<Credential, Long> {
+    Optional<Credential> getCredentialsByUserId(Long id);
 }
