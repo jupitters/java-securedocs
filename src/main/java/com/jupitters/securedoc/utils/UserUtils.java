a9524cfa31e0aa5f4d60a58c667ce2e381bde82f
jupitters <sampaiobale@gmail.com>
2025-12-09 10:43:10 -0300

    user utils

---
+++ src/main/java/com/jupitters/securedoc/utils/UserUtils.java
@@ -0,0 +1,30 @@
+package com.jupitters.securedoc.utils;
+
+import com.jupitters.securedoc.entity.Role;
+import com.jupitters.securedoc.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static java.time.LocalTime.now;
+import static jdk.internal.org.jline.utils.AttributedString.EMPTY;
+
+public class UserUtils {
+    public User createUserEntity(String firstName, String lastName, String email, Role role) {
+        return User.builder()
+                .userId(UUID.randomUUID().toString())
+                .firstName(firstName)
+                .lastName(lastName)
+                .lastLogin(LocalDateTime.from(now()))
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .enabled(false)
+                .loginAttempts(0)
+                .qrCodeSecret(String.valueOf(EMPTY))
+                .phone(String.valueOf(EMPTY))
+                .bio(String.valueOf(EMPTY))
+                .imageUrl("https://cdn-icons-png.flaticon.com/512/149/149071.png")
+                .role(role)
+                .build();
+    }
+}
