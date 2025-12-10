84a5bd65b92aa560fa00509f44f509972c956440
jupitters <sampaiobale@gmail.com>
2025-12-10 11:46:34 -0300

    user resource

--- src/main/java/com/jupitters/securedoc/resource/UserResource.java
+++ src/main/java/com/jupitters/securedoc/resource/UserResource.java
@@ -7,16 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
 import static com.jupitters.securedoc.utils.RequestUtils.getResponse;
 import static java.util.Collections.emptyMap;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -28,6 +26,12 @@ public class UserResource {
     public ResponseEntity<Response> saveUser(@RequestBody @Valid UserRequest user, HttpServletRequest request) {
         userService.createUser(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword());
         return ResponseEntity.created(getUri()).body(getResponse(request, emptyMap(), "Account Created! Check your email to enable your account.", CREATED));
+    }
+
+    @GetMapping("/verify/account")
+    public ResponseEntity<Response> verifyAccount(@RequestParam("token") String token, HttpServletRequest request) {
+        userService.verifyAccountKey(token);
+        return ResponseEntity.ok().body(getResponse(request, emptyMap(), "Account verified!", OK));
     }
 
     private URI getUri() {
