2aa5b2b2912e004e6e0d0f771e975b79365ae501
jupitters <sampaiobale@gmail.com>
2025-12-10 09:54:01 -0300

    application

--- src/main/java/com/jupitters/securedoc/SecuredocApplication.java
+++ src/main/java/com/jupitters/securedoc/SecuredocApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class SecuredocApplication {
 
 	public static void main(String[] args) {
@@ -29,6 +33,7 @@ public class SecuredocApplication {
 			adminRole.setName(Authority.ADMIN.name());
 			adminRole.setAuthorities(Authority.ADMIN);
 			roleRepository.save(adminRole);
+			RequestContext.start();
 		};
 	}
 }
