bf8a9a6269c19111b8bc2a186d015d4d8b47eb5e
jupitters <sampaiobale@gmail.com>
2025-12-10 09:10:07 -0300

    request util

--- src/main/java/com/jupitters/securedoc/utils/RequestUtils.java
+++ src/main/java/com/jupitters/securedoc/utils/RequestUtils.java
@@ -1,4 +1,16 @@
 package com.jupitters.securedoc.utils;
 
+import com.jupitters.securedoc.domain.Response;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.apache.logging.log4j.util.Strings.EMPTY;
+
 public class RequestUtils {
+    public Response getResponse(HttpServletRequest request, Map<?, ?> data, String message, HttpStatus status) {
+        return new Response(LocalDateTime.now().toString(), status.value(), request.getRequestURI(), HttpStatus.valueOf(status.value()), message, EMPTY, data);
+    }
 }
