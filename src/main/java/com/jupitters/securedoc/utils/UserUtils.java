49892405a0101328fc721c63d973d5166dafe6a6
jupitters <sampaiobale@gmail.com>
2025-12-10 11:58:26 -0300

    user utils

--- src/main/java/com/jupitters/securedoc/utils/UserUtils.java
+++ src/main/java/com/jupitters/securedoc/utils/UserUtils.java
@@ -18,6 +18,7 @@ public class UserUtils {
                 .lastLogin(LocalDateTime.from(now()))
                 .accountNonExpired(true)
                 .accountNonLocked(true)
+                .mfa(false)
                 .enabled(false)
                 .loginAttempts(0)
                 .qrCodeSecret(String.valueOf(EMPTY))
