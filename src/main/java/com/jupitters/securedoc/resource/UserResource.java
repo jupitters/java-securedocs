441a02bafa21d44e30de33e62248df1e45f20322
jupitters <sampaiobale@gmail.com>
2025-12-16 18:44:13 -0300

    login endpoint

--- src/main/java/com/jupitters/securedoc/resource/UserResource.java
+++ src/main/java/com/jupitters/securedoc/resource/UserResource.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -38,7 +39,8 @@ public class UserResource {
 
     @PostMapping("/login")
     public ResponseEntity<Response> login(@RequestBody UserRequest user) {
-        authenticationManager.authenticate();
+        authenticationManager.authenticate(UsernamePasswordAuthenticationToken
+                        .unauthenticated(user.getEmail(), user.getPassword()));
         return ResponseEntity.ok().build();
     }
 
