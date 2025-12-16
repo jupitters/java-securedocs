1933b4ab8aea6e6d145b66b92e5dceda50136540
jupitters <sampaiobale@gmail.com>
2025-12-16 11:42:41 -0300

    configuration user details service

--- src/main/java/com/jupitters/securedoc/security/FilterChainConfiguration.java
+++ src/main/java/com/jupitters/securedoc/security/FilterChainConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
@@ -14,6 +15,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public class FilterChainConfiguration {
     @Bean
     public UserDetailsService userDetailsService() {
-
+        var user1 = User.withDefaultPasswordEncoder()
+                .username("Ana")
+                .password("password")
+                .roles("USER")
+                .build();
     }
 }
