7bde2964d86b668d3a0e69af4e1d534e3295948f
jupitters <sampaiobale@gmail.com>
2025-12-16 18:14:48 -0300

    configuration user authentication manager

--- src/main/java/com/jupitters/securedoc/security/FilterChainConfiguration.java
+++ src/main/java/com/jupitters/securedoc/security/FilterChainConfiguration.java
@@ -3,6 +3,9 @@ package com.jupitters.securedoc.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +19,12 @@ import java.util.List;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class FilterChainConfiguration {
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) throws Exception {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         var user1 = User.withDefaultPasswordEncoder()
