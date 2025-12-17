b7331e4c4f03663b40b1852e05165ea0e201cc42
jupitters <sampaiobale@gmail.com>
2025-12-17 18:07:05 -0300

    new auth provider

--- src/main/java/com/jupitters/securedoc/security/NewAuthenticationProvider.java
+++ src/main/java/com/jupitters/securedoc/security/NewAuthenticationProvider.java
@@ -1,14 +1,22 @@
 package com.jupitters.securedoc.security;
 
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+@Component
+@RequiredArgsConstructor
 public class NewAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+
     @Override
     public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        var user = (UsernamePasswordA)
+        var user = (UsernamePasswordAuthenticationToken) authentication;
+
     }
 
     @Override
