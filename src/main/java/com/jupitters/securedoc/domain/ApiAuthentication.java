6887c088ca645e5b77b250fbe92503d59e31129d
jupitters <sampaiobale@gmail.com>
2025-12-17 18:36:31 -0300

    api authentication

--- src/main/java/com/jupitters/securedoc/domain/ApiAuthentication.java
+++ src/main/java/com/jupitters/securedoc/domain/ApiAuthentication.java
@@ -1,4 +1,10 @@
 package com.jupitters.securedoc.domain;
 
+import com.jupitters.securedoc.entity.User;
+
 public class ApiAuthentication {
+    private User user;
+    private String email;
+    private String password;
+    private boolean authenticated;
 }
