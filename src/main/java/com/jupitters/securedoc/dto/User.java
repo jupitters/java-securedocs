3d8e436daf57f177157f85e772171079380df965
jupitters <sampaiobale@gmail.com>
2025-12-18 09:46:44 -0300

    user dto

--- src/main/java/com/jupitters/securedoc/dto/User.java
+++ src/main/java/com/jupitters/securedoc/dto/User.java
@@ -16,11 +16,12 @@ public class User {
     private String email;
     private String phone;
     private String bio;
-    private Integer loginAttempts;
     private String imageUrl;
     private String qrCodeImageUri;
-    private String qrCodeSecret;
     private String lastLogin;
+    private String createdAt;
+    private String updatedAt;
+    private Integer loginAttempts;
     private Boolean accountNonExpired;
     private Boolean accountNonLocked;
     private Boolean enabled;
