dfc5a2ae75a481932e3ecf37bffba8123743d37f
jupitters <sampaiobale@gmail.com>
2025-12-18 11:09:23 -0300

    cache store

--- src/main/java/com/jupitters/securedoc/cache/CacheStore.java
+++ src/main/java/com/jupitters/securedoc/cache/CacheStore.java
@@ -3,6 +3,7 @@ package com.jupitters.securedoc.cache;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import jakarta.persistence.Timeout;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
@@ -16,5 +17,10 @@ public class CacheStore<K, V> {
                 .expireAfterWrite(expiryDuration, timeUnit)
                 .concurrencyLevel(Runtime.getRuntime().availableProcessors())
                 .build();
+    }
+
+    public V get(@NotNull K key) {
+        log.info("Retrieving from Cache with key {}", key.toString());
+        return cache.getIfPresent(key);
     }
 }
