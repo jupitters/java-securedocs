d3756a1b74e2980b977f581f06cc1e8124bf202d
jupitters <sampaiobale@gmail.com>
2025-12-18 10:54:32 -0300

    authentication filter

--- src/main/java/com/jupitters/securedoc/security/AuthenticationFilter.java
+++ src/main/java/com/jupitters/securedoc/security/AuthenticationFilter.java
@@ -1,9 +1,11 @@
 package com.jupitters.securedoc.security;
 
 import com.jupitters.securedoc.service.UserService;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
@@ -18,12 +20,22 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Slf4j
 public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final UserService userService;
+    private final JwtService jwtService;
+
     public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, JwtService jwtService) {
         super(new AntPathRequestMatcher("/user/login", POST.name()), authenticationManager);
+        this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
         return null;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
