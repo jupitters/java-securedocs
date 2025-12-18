80331835fa50faa534ac700c466412ea1f1b621a
jupitters <sampaiobale@gmail.com>
2025-12-18 10:56:26 -0300

    user service

--- src/main/java/com/jupitters/securedoc/service/UserService.java
+++ src/main/java/com/jupitters/securedoc/service/UserService.java
@@ -3,9 +3,11 @@ package com.jupitters.securedoc.service;
 import com.jupitters.securedoc.entity.Role;
 import com.jupitters.securedoc.entity.User;
 import com.jupitters.securedoc.enums.Authority;
+import com.jupitters.securedoc.enums.LoginType;
 
 public interface UserService {
     void createUser(String firstName, String lastName, String email, String password);
     Role getRoleName(String name);
     void verifyAccountKey(String key);
+    void updateLoginAttempt(String email, LoginType loginType);
 }
