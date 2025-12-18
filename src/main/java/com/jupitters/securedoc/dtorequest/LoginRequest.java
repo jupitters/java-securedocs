0e4ee2e7a1bfbd5e4a90ba4d49d18b670aac6895
jupitters <sampaiobale@gmail.com>
2025-12-18 11:26:19 -0300

    login request

--- src/main/java/com/jupitters/securedoc/dtorequest/LoginRequest.java
+++ src/main/java/com/jupitters/securedoc/dtorequest/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.jupitters.securedoc.dtorequest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginRequest {
     @Email(message = "Invalid email address!")
     private String email;
