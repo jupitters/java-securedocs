031732580900af01d4b7cb2e3303142594842c01
jupitters <sampaiobale@gmail.com>
2025-12-18 10:51:06 -0300

    authentication filter

--- src/main/java/com/jupitters/securedoc/security/AuthenticationFilter.java
+++ src/main/java/com/jupitters/securedoc/security/AuthenticationFilter.java
@@ -1,4 +1,29 @@
 package com.jupitters.securedoc.security;
 
-public class AuthenticationFilter {
+import com.jupitters.securedoc.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.AntPathMatcher;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpMethod.POST;
+
+@Slf4j
+public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, JwtService jwtService) {
+        super(new AntPathRequestMatcher("/user/login", POST.name()), authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        return null;
+    }
 }
