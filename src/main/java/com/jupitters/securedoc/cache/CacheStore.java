3b71784ac6a27d43a7b9b99cfb2318ce3b8d0e09
jupitters <sampaiobale@gmail.com>
2025-12-18 11:07:28 -0300

    cache store constructor

--- src/main/java/com/jupitters/securedoc/cache/CacheStore.java
+++ src/main/java/com/jupitters/securedoc/cache/CacheStore.java
@@ -1,8 +1,20 @@
 package com.jupitters.securedoc.cache;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import jakarta.persistence.Timeout;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class CacheStore<K, V> {
+    private final Cache<K, V> cache;
 
+    public CacheStore(int expiryDuration, TimeUnit timeUnit) {
+        cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expiryDuration, timeUnit)
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .build();
+    }
 }
